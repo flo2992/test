e0038bf8a99e97a4c90f8093cc2fd49ac98ff5fc
Alexandre <zebboudjalexandre@gmail.com>
2020-04-22 16:03:02 +0200

    seed image

--- database/seeds/DemoDataSeeder.php
+++ database/seeds/DemoDataSeeder.php
@@ -13,12 +13,27 @@ class DemoDataSeeder extends Seeder
      */
     public function run()
     {
+        $path = './public/uploads/rce2.jpg';
+        $image = file_get_contents($path);
+        // $base64 = base64_encode($logo);
+
         DB::table('users')->insert([
             'name'     => 'John',
+            'image'     => $image,
             'email'    => 'john@doe.com',
             'avatar'   => null,
             'password' => bcrypt('password'),
         ]);
+
+        
+            
+            
+            
+
+        
+
+        
+            
 
         factory(Event::class, 50)->create();
     }
